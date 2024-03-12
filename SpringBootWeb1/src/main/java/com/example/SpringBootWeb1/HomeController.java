14c133d0fba0929f6898ba3883f468c7143401d7
Hp <adkhamjon.abd@gmail.com>
2024-03-12 20:03:45 +0100

    Adds Prefixes and Suffixes

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -4,7 +4,9 @@ import com.sun.net.httpserver.HttpServer;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -12,17 +14,14 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session){
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
 
-        int num1  = Integer.parseInt(req.getParameter("num1"));
-        int num2  = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
-        System.out.println(result);
-        session.setAttribute("result", result);
-        return "result.jsp";
+        model.addAttribute("result", result);
+        return "result";
     }
 }
