93ce8e4c7b1c2aacc50b439c346d92e2f8a36a8c
Hp <adkhamjon.abd@gmail.com>
2024-03-17 11:03:31 +0100

    Adds methods to data class

--- JobApp/src/main/java/com/example/JobApp/service/JobService.java
+++ JobApp/src/main/java/com/example/JobApp/service/JobService.java
@@ -1,5 +1,16 @@
 package com.example.JobApp.service;
 
+import com.example.JobApp.model.JobPost;
+
+import java.util.List;
+
 public class JobService {
-    
+    public void addJob(){
+
+    }
+
+    public List<JobPost> getAllJobs(){
+
+    }
+
 }
