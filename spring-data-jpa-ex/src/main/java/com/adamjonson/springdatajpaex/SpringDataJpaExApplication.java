a585012b6bd12ab61bcbe9739b3da731fec0b6a9
Hp <adkhamjon.abd@gmail.com>
2024-03-23 14:30:24 +0100

    Create an new Student objects in jpa project

--- spring-data-jpa-ex/src/main/java/com/adamjonson/springdatajpaex/SpringDataJpaExApplication.java
+++ spring-data-jpa-ex/src/main/java/com/adamjonson/springdatajpaex/SpringDataJpaExApplication.java
@@ -1,13 +1,31 @@
 package com.adamjonson.springdatajpaex;
 
+import com.adamjonson.springdatajpaex.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDataJpaExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaExApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringDataJpaExApplication.class, args);
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+
+		s1.setRollNo(101);
+		s1.setName("Mario");
+		s1.setMarks(75);
+
+		s1.setRollNo(101);
+		s1.setName("Mario");
+		s1.setMarks(75);
+
+		s1.setRollNo(101);
+		s1.setName("Mario");
+		s1.setMarks(75);
+
 	}
 
 }
