a6a9210c99be9da6cfcd9778e0ec094a0018bd14
Hp <adkhamjon.abd@gmail.com>
2024-03-28 00:13:31 +0100

    Add new Spring JDBC project

---
+++ Spring.JDBCEx/src/main/java/com/adamjonson/Spring/JDBCEx/Application.java
@@ -0,0 +1,13 @@
+package com.adamjonson.Spring.JDBCEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
