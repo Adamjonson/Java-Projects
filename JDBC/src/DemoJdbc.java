255b5b6e1e0f1445d3c7a65578947ea9e57b132a
Hp <adkhamjon.abd@gmail.com>
2024-03-31 11:38:24 +0200

    Executes a simple query

--- JDBC/src/DemoJdbc.java
+++ JDBC/src/DemoJdbc.java
@@ -6,10 +6,16 @@ public class DemoJdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "0000";
+        String sql = "Select sname from student where sid=1";
 
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
-
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        rs.next();
+        String name = rs.getString("sname");
+        System.out.println("Name of the student is " + name);
+        con.close();
         System.out.println("Connections established");
 
     }
