9b1099b5a719532a8af13a0948973841669117af
Hp <adkhamjon.abd@gmail.com>
2024-03-31 13:50:12 +0200

    Replace the simple Statement query with PreparedStatment query

--- JDBC/src/DemoJdbc.java
+++ JDBC/src/DemoJdbc.java
@@ -3,19 +3,22 @@ import org.postgresql.Driver;
 import java.sql.*;
 public class DemoJdbc {
     public static void main(String[] args) throws Exception {
-        int sid = 101;
-        String sname = "Max";
+        int sid = 102;
+        String sname = "Julia";
         int marks = 48;
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "0000";
-        String sql = "insert into student values (" + sid + ", '" + sname + "', " +  marks + ")";
+        String sql = "insert into student values (?, ?, ?)";
 
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
-        Statement st = con.createStatement();
-        boolean status = st.execute(sql);
-        System.out.println(status);
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1, sid);
+        st.setString(2, sname);
+        st.setInt(3, marks);
+        st.execute();
+        System.out.println();
         con.close();
         System.out.println("Connections established");
 
