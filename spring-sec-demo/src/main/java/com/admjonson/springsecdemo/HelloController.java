535f11b3b950f2fbbd6ebbda64a0c1df05c6a6ef
Hp <adkhamjon.abd@gmail.com>
2024-04-02 20:53:31 +0200

    Add small change to print session id

--- spring-sec-demo/src/main/java/com/admjonson/springsecdemo/HelloController.java
+++ spring-sec-demo/src/main/java/com/admjonson/springsecdemo/HelloController.java
@@ -1,13 +1,18 @@
 package com.admjonson.springsecdemo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("hello")
-    public String greet(){
-        return "Hello World";
+    public String greet(HttpServletRequest request) {
+        return "Hello World " + request.getSession().getId();
     }
 
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Adam " + request.getSession().getId();
+    }
 }
