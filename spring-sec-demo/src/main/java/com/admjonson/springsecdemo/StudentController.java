9f3ee0290b6d3493f5fb41fa790b69b17d32a9b3
Hp <adkhamjon.abd@gmail.com>
2024-04-03 11:05:12 +0200

    Add lombok and controller

---
+++ spring-sec-demo/src/main/java/com/admjonson/springsecdemo/StudentController.java
@@ -0,0 +1,25 @@
+package com.admjonson.springsecdemo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Adam", "Java"),
+            new Student(2, "Kate", "Blockchain")
+    ));
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+}
